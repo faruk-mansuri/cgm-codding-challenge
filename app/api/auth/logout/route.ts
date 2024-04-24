6fea314924eac2b25ce8f7522432a507f2bf0567
faruq <faruqmansuri707@gmail.com>
2024-04-24 19:59:15 +0530

    fix user logout api

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -8,10 +8,12 @@ export const GET = () => {
       { status: StatusCodes.OK }
     );
 
-    response.cookies.set('token', 'logout', {
-      httpOnly: true,
-      expires: new Date(0),
-    });
+    // response.cookies.set('token', 'logout', {
+    //   httpOnly: true,
+    //   expires: new Date(0),
+    // });
+
+    response.cookies.delete('token');
 
     return response;
   } catch (error) {
